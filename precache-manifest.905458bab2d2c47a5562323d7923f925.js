0cdc968ff286cfa2152f6b4ff6590b46f096df42
DevNavin <snavindev2@gmail.com>
2023-03-30 20:21:23 -0700

    Updates

---
+++ precache-manifest.905458bab2d2c47a5562323d7923f925.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46235d04c7bb311a67f460b43a3ebe79",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "909fbc0b3e10ece173d8",
+    "url": "/website/static/css/2.f00fa05b.chunk.css"
+  },
+  {
+    "revision": "efb9da0aef4774fc3e01",
+    "url": "/website/static/css/main.2ac42fda.chunk.css"
+  },
+  {
+    "revision": "909fbc0b3e10ece173d8",
+    "url": "/website/static/js/2.4fce5520.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/website/static/js/2.4fce5520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb9da0aef4774fc3e01",
+    "url": "/website/static/js/main.ecc0d8f4.chunk.js"
+  },
+  {
+    "revision": "32a424006b7f259b7b10",
+    "url": "/website/static/js/runtime-main.a466b34d.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "/website/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/website/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "f8cb4528a82ce112d46b3f34069d1eaa",
+    "url": "/website/static/media/books read.f8cb4528.png"
+  },
+  {
+    "revision": "4e02b8ca661234658f57681d0a5ad1bf",
+    "url": "/website/static/media/employee polls.4e02b8ca.png"
+  },
+  {
+    "revision": "87561258a0fb17472008a4b2ff66696f",
+    "url": "/website/static/media/laptop1.87561258.jpg"
+  },
+  {
+    "revision": "54f5f4e755ed7be3ce2a372f9f727b7b",
+    "url": "/website/static/media/musicplayer.54f5f4e7.png"
+  },
+  {
+    "revision": "34abf38813a1264a0605ea7d55d47ed4",
+    "url": "/website/static/media/sam.34abf388.jpg"
+  },
+  {
+    "revision": "3255acb8e9d5330bf6364a48a87ca13d",
+    "url": "/website/static/media/stock_tracker_thumbnail.3255acb8.png"
+  },
+  {
+    "revision": "1fb3a84eec6f26c3e25ea0ed80b3d12a",
+    "url": "/website/static/media/tic_tac_thumbnail.1fb3a84e.png"
+  },
+  {
+    "revision": "8156351872e27267a44cc9d4e3d674d4",
+    "url": "/website/static/media/weather_updates.81563518.png"
+  }
+]);
